661af2f48f125ac2b371ccf5171a847edb7a5b7f
AnaTorres61 <ana.torres61@aluno.ifce.edu.br>
2023-08-10 15:19:08 -0300

    destroy shot by distance

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,13 +1,18 @@
 extends Area2D
 # class member variables go here
+const MAX_DIST = 250
+
 var dir = Vector2(0,-1) setget set_dir
 var vel = 300
+onready var init_pos = global_position
 
 func _ready():
-	
 	pass
 
 func _process(delta):
+	if global_position.distance_to(init_pos) >= MAX_DIST:
+		queue_free()
+	
 	translate(dir * vel * delta)
 
 # delete on screen exit
