85d2c47870618022b9b9f5eea8941033e997d050
AnaTorres61 <ana.torres61@aluno.ifce.edu.br>
2023-08-11 10:03:40 -0300

    bullet self-collision effect

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -6,14 +6,22 @@ var dir = Vector2(0,-1) setget set_dir
 var vel = 300
 onready var init_pos = global_position
 
+var live = true
+
 func _ready():
 	pass
 
 func _process(delta):
-	if global_position.distance_to(init_pos) >= MAX_DIST:
-		queue_free()
-	
-	translate(dir * vel * delta)
+	if live:
+		if global_position.distance_to(init_pos) >= MAX_DIST:
+			$smoke.emitting = false
+			live = false
+			$sprite.visible = false
+			$anim_self_destruction.play("explode")
+			yield($anim_self_destruction, "animation_finished")
+			queue_free()
+		
+		translate(dir * vel * delta)
 
 # delete on screen exit
 func _on_notifier_screen_exited():
