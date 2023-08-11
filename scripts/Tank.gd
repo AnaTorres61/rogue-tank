6e47386b157295169536845d0e81564254443eea
AnaTorres61 <ana.torres61@aluno.ifce.edu.br>
2023-08-11 15:13:58 -0300

    tank accelerator

--- scripts/Tank.gd
+++ scripts/Tank.gd
@@ -6,8 +6,9 @@ onready var BULLET_TANK_GROUP = "bullet-" + str(self)
 
 const ROT_VEL = PI/2 # 1/4 volta por seg
 
-var speed = 200
+const MAX_SPEED = 200
 var pre_bullet = preload("res://scenes/bullet.tscn")
+var acell = 0
 
 export(int, "bigRed", "blue", "dark", "darkLarge", "green", "huge", "red", "sand") var bodie = 2 setget set_bodie
 export(int, "dark1", "dark2", "dark3", "green1", "green2", "green3", "red1", "red2", "red3", "sand1", "sand2", "sand3") var barrel = 0 setget set_barrel
@@ -67,30 +68,43 @@ func _physics_process(delta):
 #	if Input.is_action_pressed("ui_down"):
 #		dir_y += 1
 ##	shoot move
-#	if Input.is_action_just_pressed("ui_shoot"):
-##		if there are up to three bullets in the group
-#		if get_tree().get_nodes_in_group(BULLET_TANK_GROUP).size() < 6:
-#			var bullet = pre_bullet.instance()
-#			# put the bullet on the tip cannon
-#			bullet.global_position = $barrel/muzzle.global_position
-#			bullet.dir = Vector2(cos(rotation), sin(rotation)).normalized()
-#			bullet.add_to_group(BULLET_TANK_GROUP)
-#			# put the bullet in the scene
-#			get_parent().add_child(bullet) # $"../".add_child(bullet)
-#			$barrel/anim.play("fire")
+	if Input.is_action_just_pressed("ui_shoot"):
+		# if there are up to three bullets in the group
+		if get_tree().get_nodes_in_group(BULLET_TANK_GROUP).size() < 6:
+			var bullet = pre_bullet.instance()
+			# put the bullet on the tip cannon
+			bullet.global_position = $barrel/muzzle.global_position
+			bullet.dir = Vector2(cos(rotation), sin(rotation)).normalized()
+			bullet.add_to_group(BULLET_TANK_GROUP)
+			# put the bullet in the scene
+			get_parent().add_child(bullet) # $"../".add_child(bullet)
+			$barrel/anim.play("fire")
 #
 #	look_at(get_global_mouse_position())
 #
 #	move_and_slide(Vector2(dir_x,dir_y) * speed)
 	
 	var rot = 0
+	var dir = 0
 	
 	if Input.is_action_pressed("ui_right"):
 		rot +=1
 	if Input.is_action_pressed("ui_left"):
 		rot -=1
+	if Input.is_action_pressed("ui_up"):
+		dir += 1
+	if Input.is_action_pressed("ui_down"):
+		dir -= 1
 		
 	rotate(ROT_VEL * rot * delta)
+	
+	if dir != 0:
+		acell = lerp(acell, MAX_SPEED*dir, .03)
+	else:
+		acell = lerp(acell, 0, .05)
+	
+	move_and_slide(Vector2(cos(rotation), sin(rotation)) * acell)
+	
 	
 func set_bodie(val):
 	bodie = val
