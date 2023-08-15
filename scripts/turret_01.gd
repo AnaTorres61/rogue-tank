22a8886d13f6dd7d4b8ab558cdb587e97b510b3d
AnaTorres61 <ana.torres61@aluno.ifce.edu.br>
2023-08-15 20:58:35 -0300

    add first enemy (turret)

---
+++ scripts/turret_01.gd
@@ -0,0 +1,20 @@
+extends StaticBody2D
+
+
+
+func _ready():
+
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_sensor_body_entered(body):
+	pass # replace with function body
+
+
+func _on_sensor_body_exited(body):
+	pass # replace with function body
