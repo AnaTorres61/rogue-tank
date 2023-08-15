8aef8f9f729b7f7e791b0d0353beb64f96337525
AnaTorres61 <ana.torres61@aluno.ifce.edu.br>
2023-08-15 19:20:17 -0300

    spread oil on the track

--- scripts/barrelBlack_top.gd
+++ scripts/barrelBlack_top.gd
@@ -1,5 +1,6 @@
 extends StaticBody2D
 
+const PRE_OIL = preload("res://scenes/oilSpill_large.tscn")
 
 
 func _ready():
@@ -12,4 +13,16 @@ func on_area_hitted(damage, health, node):
 
 
 func on_area_destroid():
+	var oil_count = rand_range(1,4)
+	
+	for o in range(oil_count):
+		var oil = PRE_OIL.instance()
+		var angle = randf() * (PI * 2)
+		oil.global_position = global_position + Vector2(cos(angle), sin(angle)).normalized() * rand_range(30,60)
+		$"../".add_child(oil)
+	
+	$area_obstacle.queue_free()
+	$shape.queue_free()
+	$anim.play("explode")
+	yield($anim, "animation_finished")
 	queue_free()
