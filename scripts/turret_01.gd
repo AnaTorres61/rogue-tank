6787449677c4826e5b23e8da041a22a6d44f3b82
AnaTorres61 <ana.torres61@aluno.ifce.edu.br>
2023-08-19 21:04:12 -0300

    turret laser sight

--- scripts/turret_01.gd
+++ scripts/turret_01.gd
@@ -2,12 +2,11 @@ extends StaticBody2D
 
 var bodies = []
 
-var rot_vel = PI * 1.5
+var rot_vel = PI/2
 
 var PRE_BULLET = preload("res://scenes/turret_01_bullet.tscn")
 
 func _ready():
-
 	pass
 
 func _process(delta):
@@ -19,17 +18,24 @@ func _process(delta):
 
 
 func _on_sensor_body_entered(body):
+	if !bodies.size():
+		$shoot_timer.start()
 	bodies.append(body)
+	$cannon/sight.enabled = true
 
 
 func _on_sensor_body_exited(body):
 	var index = bodies.find(body)
 	if index >= 0:
 		bodies.remove(index)
+	if !bodies.size():
+		$cannon/sight.enabled = false
+		$shoot_timer.stop()
 
 
-func _on_Timer_timeout():
-	var bullet = PRE_BULLET.instance()
-	bullet.global_position = global_position
-	bullet.dir = Vector2(cos($cannon.rotation), sin($cannon.rotation))
-	get_parent().add_child(bullet)
+func _on_shoot_timer_timeout():
+	if $cannon/sight.is_colliding():
+		var bullet = PRE_BULLET.instance()
+		bullet.global_position = global_position
+		bullet.dir = Vector2(cos($cannon.rotation), sin($cannon.rotation))
+		get_parent().add_child(bullet)
